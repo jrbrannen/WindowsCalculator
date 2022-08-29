9d3127a1694b13db6a0265e924243790e0db7a89
jrbrannen <jeremybrannen@gmail.com>
2022-08-28 20:41:57 -0500

    Added new form to use table grid view

---
+++ WindowsCalculator/CalculatorForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsCalculator
+{
+    public partial class CalculatorForm : Form
+    {
+        DisplayScreen displayScreen;
+        public CalculatorForm()
+        {
+            InitializeComponent();
+            displayScreen = new DisplayScreen(this);
+            OpenDisplayScreen();
+        }
+
+        public void OpenDisplayScreen()
+        {
+            this.Controls.Clear();
+            //DisplayScreen displayScreen = new DisplayScreen(this);
+            this.Controls.Add(displayScreen);
+        }
+    }
+}
